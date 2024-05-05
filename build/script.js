8b511962a3406792df7b5c6b5d7cf21828dd847d
prithvikr66 <pkunofficial66@gmail.com>
2024-05-05 23:56:45 +0530

    fixes

--- build/script.js
+++ build/script.js
@@ -1,14 +1,13 @@
 const { exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
-const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { lookup } = require("mime-types");
 const { config } = require("dotenv");
-const archiever = require("archiver")
-const unzipper = require("unzipper");
 
 config();
 
+const PROJECT_ID = process.env.PROJECT_ID;
 const s3Client = new S3Client({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -17,61 +16,33 @@ const s3Client = new S3Client({
   region: process.env.AWS_S3_REGION,
 });
 
-// const zipFolder = async(srcFilePath , outputFilePath) =>{
+const init = async () => {
+  const outDirPath = path.join(__dirname, "output");
+  const build = exec(`cd ${outDirPath} && npm install && npm run build`);
 
-//   try{
-//     const outputFile = fs.createWriteStream(outputFilePath);
-//     const archieve = archiever("zip" , {
-//       zlib:{
-//         level:9
-//       }
-//     });
+  build.stdout.on("error", (err) => console.log(err));
+  build.stdout.on("data", (data) => console.log(data));
+  build.on("close", async () => {
+    const distFolderPath = path.join(__dirname, "output", "dist");
 
-//     outputFile.on("close",()=>console.log("Archieve has been completed"));
-//     archieve.pipe(outputFile);
-//     archieve.directory(srcFilePath,false);
-//     archieve.finalize();
-//     archieve.on("error",(err)=>console.log(err))
+    const folders = fs.readdirSync(distFolderPath, { recursive: true });
 
-//   }catch(err) {
-//     console.log(err)
-// }
-// }
-// const unzippedFile = fs.createReadStream(path.join(__dirname , "output", "compressed.zip")).pipe(unzipper.Extract({path:path.join(__dirname , "output" , "unzipped")}))
+    for (const file of folders) {
+      const filePath = path.join(distFolderPath, file);
+      if (fs.lstatSync(filePath).isDirectory()) continue;
 
+      const command = new PutObjectCommand({
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: `${PROJECT_ID}/${file}`,
+        Body: fs.createReadStream(
+          path.join(__dirname, "output", "compressed.zip")
+        ),
+        ContentType: lookup(path.join(__dirname, "output", "compressed.zip")),
+      });
 
-const init = async() => {
-  //  const outDirPath = path.join(__dirname , "output");
-  //  const build = exec(`cd ${outDirPath} && npm install && npm run build` );
-
-  //  build.stdout.on("error",(err)=>console.log(err))
-  //  build.stdout.on("data",(data)=>console.log(data))
-  //  build.on("close",()=>{
-  //     console.log("Build Complete")
-  //  })
-
-  const distFolderPath = path.join(__dirname, "output", "dist");
-  await zipFolder(distFolderPath , path.join(__dirname , "output" , "compressed.zip"));
-  
-  const folders = fs.readdirSync(distFolderPath, { recursive: true });
-
-  for (const file of folders) {
-    const filePath = path.join(distFolderPath, file);
-    if (fs.lstatSync(filePath).isDirectory()) continue;
-
-    const command = new PutObjectCommand({
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: `ouput/compressed.zip`,
-      Body: fs.createReadStream(path.join(__dirname , "output" ,"compressed.zip")),
-      ContentType: lookup(path.join(__dirname , "output" ,"compressed.zip")),
-    });
-
-    const upload = await s3Client.send(command)
-    console.log(`${filePath.split("dist/")[1]} file uploaded`)
-
-    
-    
-  }
-
+      await s3Client.send(command);
+    }
+    console.log("Build Complete");
+  });
 };
 init();
